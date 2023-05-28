eee70d07f55df396fc7d3f72a29b49b878ae8507
danjkim21 <dan.jkim21@gmail.com>
2023-05-28 10:04:51 -0400

    initial bookmarks and tasks page styling and rendering data

--- pages/dashboard/bookmarks/index.js
+++ pages/dashboard/bookmarks/index.js
@@ -1,13 +1,35 @@
 import DashboardLayout from "../Layout";
 import useGetBookmarks from "../../../hooks/useGetBookmarks.js";
+import Link from "next/link";
 
 const Bookmarks = () => {
   const { bookmarks, setBookmarks, error, status } = useGetBookmarks();
-  console.log(bookmarks, status);
+  let bookmarkItems;
+
+  if (bookmarks && status === 200) {
+    bookmarkItems = bookmarks.map((bookmark) => {
+      return (
+        <article className="w-full" key={bookmark.id}>
+          <Link href={bookmark.link}>
+            <span className="text-cyan-300">{`<a>`}</span>
+            <span>{bookmark.title}</span>
+            <span className="text-cyan-300">{`</a>`}</span>
+          </Link>
+        </article>
+      );
+    });
+  }
 
   return (
     <DashboardLayout>
-      <>Bookmarks page</>
+      <>
+        <h1>
+          <span className="text-purple-300">{`<h1>`}</span>
+          <span>Bookmarks</span>
+          <span className="text-purple-300">{`</h1>`}</span>
+        </h1>
+        {bookmarkItems}
+      </>
     </DashboardLayout>
   );
 };
--- pages/dashboard/tasks/index.js
+++ pages/dashboard/tasks/index.js
@@ -4,11 +4,30 @@ import useGetTasks from "@/hooks/useGetTasks";
 const Tasks = () => {
   const { tasks, setTasks, error, status } = useGetTasks();
 
-  console.log(tasks);
+  let taskItems;
+
+  if (tasks && status === 200) {
+    taskItems = tasks.map((task) => {
+      return (
+        <section key={task.id}>
+          <span className="text-cyan-300">{`<li>`}</span>
+          <span>{task.title}</span>
+          <span className="text-cyan-300">{`</li>`}</span>
+        </section>
+      );
+    });
+  }
 
   return (
     <DashboardLayout>
-      <>Tasks page</>
+      <>
+        <h1>
+          <span className="text-purple-300">{`<h1>`}</span>
+          <span>Tasks</span>
+          <span className="text-purple-300">{`</h1>`}</span>
+        </h1>
+        {taskItems}
+      </>
     </DashboardLayout>
   );
 };
